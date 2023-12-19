7a75a5ee6dc027860d4a1d602ed8bd058630466d
LucasBebek <bebeklucas7@gmail.com>
2023-12-19 19:34:57 -0300

    .

--- src/app/componentes/stock/agregar-producto/agregar-producto.component.ts
+++ src/app/componentes/stock/agregar-producto/agregar-producto.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Categoria } from 'src/app/models/categoria';
 import { Producto } from 'src/app/models/producto';
 import { CategoriaService } from 'src/app/services/categoria.service';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-agregar-producto',
@@ -9,10 +10,10 @@ import { CategoriaService } from 'src/app/services/categoria.service';
   styleUrls: ['./agregar-producto.component.css']
 })
 export class AgregarProductoComponent implements OnInit{
-    producto!: Producto 
+    producto: Producto = new Producto(0,'',0,0,''); 
     categorias : Categoria[] = [];
     
-    constructor(private categoriaService: CategoriaService){}
+    constructor(private categoriaService: CategoriaService, private productoService: ProductoService){}
 
     ngOnInit(): void {
       this.obtenerLasCategorias();
--- src/app/models/producto.ts
+++ src/app/models/producto.ts
@@ -1,12 +1,26 @@
-import { Categoria } from "./categoria";
+
 
 export class Producto{
+         id: number;
+         nombre: string;
+         precio: number;
+         
+         stock: number;
+         categoria_name: String;
+    
     constructor(
-        public id: number,
-        public nombre: string,
-        public precio: number,
-        public precio_unitario: number,
-        public stock: number,
-        public categoria_name: String
-    ){}
+         id: number,
+        nombre: string,
+        precio: number,
+        
+        stock: number,
+        categoria_name: String
+        ){
+            this.id = id;
+            this.nombre = nombre;
+            this.precio = precio;
+           
+            this.stock = stock;
+            this.categoria_name = categoria_name;
+        }
 }
